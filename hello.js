c4a288e0c640240305e9225ce881ac10cf1521b6
SertaçKarahoda <skarahoda@sabanciuniv.edu>
2017-07-17 15:42:38 +0300

    sertaç

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@
  * Created by SertaçKarahoda on 17-Jul-17.
  */
 console.log("merhaba");
-console.log("sertaç");
+console.log("sertaç karahoda");
 for(var i= 0; i < 10 ; i++){
 	console.log("i:", i);
 }

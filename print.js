08767e94c826e62b2a93c4494cdf18a1d77b861f
SertaçKarahoda <skarahoda@sabanciuniv.edu>
2017-07-17 16:01:51 +0300

    initial print file

--- print.js
+++ print.js
@@ -1,12 +1,29 @@
-/**
- * Created by SertaçKarahoda on 17-Jul-17.
- */
 var rl = require('readline');
 
 var i = rl.createInterface(process.stdin, process.stdout, null);
-i.question('Write your name: ', function(answer) {
-	console.log('Nice to meet you> ' + answer);
+
+function cevaplayici(cevap){
+	console.log('Nice to meet you> ' + cevap);
 	i.close();
 	process.stdin.destroy();
+}
+i.question('Write your name: ', cevaplayici);
 
-});
+
+var a = {
+	foo: function(){
+		console.log("foo");
+	},
+	bar: function(){
+		console.log("bar");
+	},
+	hello: "world",
+	myNum: 12321
+};
+
+
+a.foo();
+
+a.bar();
+
+console.log(a.hello);
